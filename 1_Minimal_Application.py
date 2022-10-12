d062e8e5c7dbb508c6c7ec0808a8dc5de7e67e36
sundas586 <33677647+sundas586@users.noreply.github.com>
2022-10-11 19:31:17 -0700

    Update 1_Minimal_Application.py

--- 1_Minimal_Application.py
+++ 1_Minimal_Application.py
@@ -1,13 +1,13 @@
-
 # A minimal Flask application looks something like this:
 
-from flask import Flask    # import Flask class from flask module
+from flask import Flask # import Flask class from flask module
 
-app = Flask(__name__)      # Define App
+app = Flask(__name__)
 
-@app.route("/myFlaskWebApp")            # End-point of our web-app
-def hello_world():         # function to be run when any user visits the end-point of my web-app
-    return "<p>Hello, World!</p>
-  
-app.run(debug= True)                  # run on local host # auto Update
-  
+@app.route("/")     # End-point of our web-app
+def firstpage():      # function to be run when any user visits the end-point of my web-app
+    return "<p>Hello, Welcome to Home Page!</p>"
+@app.route("/FlaskWebApp")
+def secondpage():
+    return "<p>Hello, This is second page!</p>"
+app.run(debug=True)     # # run on local host # auto Update
