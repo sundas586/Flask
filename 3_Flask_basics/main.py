c007d4fc3b05291490dee562916552e44e7465b5
sundas586 <33677647+sundas586@users.noreply.github.com>
2022-10-14 07:59:12 -0700

    Create main.py

---
+++ 3_Flask_basics/main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template # import Flask class from flask module
+
+myapp = Flask(__name__)
+
+@myapp.route("/<job>")                   # End-point of our web-app
+def FlaskTut(job):                       # function to be run when any user visits the end-point of my web-app
+    name = "Sundas"
+    return render_template("index.html", name1 = name, profession = str(job))
+
+myapp.run(debug=True)                    # run on local host # auto Update
